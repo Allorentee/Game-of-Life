18d63c96294b77b96732c67fcb7616696fbf3a56
AlvaroLlorente <Allorentee@gmail.com>
2022-10-15 19:14:59 +0200

    Pruebas SonarClound

---
+++ modules/chngeBoards.js
@@ -0,0 +1,5 @@
+import { gameOfLife } from './board.js';
+// import { changingCellStatus } from 'chngeCellStatus.js';
+// import { aliveDataBaseNeighbours, deathDataBaseNeighbours } from 'data-Base.js';
+
+console.table(gameOfLife);
