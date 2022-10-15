15ac7da50b810e76de4f338cccc19c4d485e3abe
AlvaroLlorente <Allorentee@gmail.com>
2022-10-15 20:09:25 +0200

    Añadiendo loop cambio de tablero

--- modules/chngeBoards.js
+++ modules/chngeBoards.js
@@ -1,5 +1,37 @@
 import { gameOfLife } from './board.js';
-// import { changingCellStatus } from 'chngeCellStatus.js';
-// import { aliveDataBaseNeighbours, deathDataBaseNeighbours } from 'data-Base.js';
+import { changingCellStatus } from './chngeCellStatus.js';
+import {
+    aliveDataBaseNeighbours,
+    deathDataBaseNeighbours,
+} from './data-Base.js';
 
-console.table(gameOfLife);
+export const boardLoop = (board) => {
+    for (let i = 0; i < 10; i++) {
+        let centinel = true;
+
+        if (centinel) {
+            const board3 = changingCellStatus(board);
+            let dataBaseAlive = aliveDataBaseNeighbours(board3);
+            let dataBaseDeath = deathDataBaseNeighbours(board3);
+            changingCellStatus(board3, dataBaseAlive, dataBaseDeath);
+            centinel = false;
+            console.table(
+                changingCellStatus(board3, dataBaseAlive, dataBaseDeath)
+            );
+        }
+        if (!centinel) {
+            const board2 = changingCellStatus(board);
+            let dataBaseAlive = aliveDataBaseNeighbours(board2);
+            let dataBaseDeath = deathDataBaseNeighbours(board2);
+            changingCellStatus(board2, dataBaseAlive, dataBaseDeath);
+            centinel = true;
+            console.table(
+                changingCellStatus(board2, dataBaseAlive, dataBaseDeath)
+            );
+        }
+    }
+};
+
+setInterval(function () {
+    boardLoop(gameOfLife);
+}, 1000);
--- modules/chngeCellStatus.js
+++ modules/chngeCellStatus.js
@@ -1,19 +1,18 @@
-import { gameOfLife } from './board.js';
 import {
     aliveDataBaseNeighbours,
     deathDataBaseNeighbours,
 } from './data-Base.js';
 
-export const changingCellStatus = (board) => {
-    const dataDeathItems = deathDataBaseNeighbours(board);
-    const dataAliveItems = aliveDataBaseNeighbours(board);
+export const changingCellStatus = (board, bdv, bdm) => {
+    bdm = deathDataBaseNeighbours(board);
+    bdv = aliveDataBaseNeighbours(board);
     const board2 = board;
-    dataDeathItems.forEach((deathItem) => {
+    bdm.forEach((deathItem) => {
         if (deathItem.countN === 3) {
             board2[deathItem.r][deathItem.c] = 1;
         }
     });
-    dataAliveItems.forEach((aliveItem) => {
+    bdv.forEach((aliveItem) => {
         if (aliveItem.countN < 2) {
             board2[aliveItem.r][aliveItem.c] = 0;
         }
@@ -23,5 +22,3 @@ export const changingCellStatus = (board) => {
     });
     return board2;
 };
-
-console.table(changingCellStatus(gameOfLife));
