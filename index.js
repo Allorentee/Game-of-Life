bbd8f6754125e8e003d3a1fae699180308306e89
AlvaroLlorente <Allorentee@gmail.com>
2022-10-15 09:45:21 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+// const gameOfLife = [
+//     [0, 1, 0],
+//     [0, 1, 0],
+//     [0, 0, 0],
+// ];
+
+// console.table(gameOfLife);
+
+// let test = [0, 1, 0, 1, 0, 1, 0, 1, 0];
+
+// const killTheCell = (array = []) => {
+//     let indices = [];
+//     j = 0;
+//     for (let i = 0; i < array.length; i++) {
+//         if (array[i] === 1) {
+//             indices.push(i);
+//         }
+//         if (array[indices[j] + 1] === 0 && array[indices[j] - 1] === 0) {
+//             array[indices[j]] = 0;
+//             j++;
+//         }
+//     }
+//     return array;
+// };
+// console.log(killTheCell(test));
