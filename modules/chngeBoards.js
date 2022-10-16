628fd9447430e3bef1d4c217b7bd576dcccb4cca
AlvaroLlorente <Allorentee@gmail.com>
2022-10-16 15:19:26 +0200

    Refactor chnge-Board function

--- modules/chngeBoards.js
+++ modules/chngeBoards.js
@@ -1,21 +1,9 @@
 import { changingCellStatus } from './chngeCellStatus.js';
 
 export const boardLoop = (board) => {
-    console.table(board);
-    let board2 = changingCellStatus(board);
-    let centinel = true;
     for (let i = 0; i < 1; i++) {
-        if (centinel) {
-            console.table(board);
-            board2 = changingCellStatus(board);
-            centinel = false;
-            continue;
-        }
-
-        if (!centinel) {
-            console.table(board2);
-            board = changingCellStatus(board2);
-            centinel = true;
-        }
+        console.table(board);
+        board = changingCellStatus(board);
+        continue;
     }
 };
