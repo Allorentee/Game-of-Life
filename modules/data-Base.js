9b7a1e0527b2ed27889fc3de4c408d16a1a0489d
AlvaroLlorente <Allorentee@gmail.com>
2022-10-16 14:58:59 +0200

    Final game with emogis

--- modules/data-Base.js
+++ modules/data-Base.js
@@ -8,16 +8,16 @@ export const aliveDataBaseNeighbours = (array) => {
     let dataBaseAliveCells = [];
     for (let i = 1; i < array.length - 1; i++) {
         for (let j = 1; j < array.length + 1; j++) {
-            if (array[i][j] === 1) {
+            if (array[i][j] === '💥') {
                 let count = 0;
-                if (array[i - 1][j] === 1) count++;
-                if (array[i + 1][j] === 1) count++;
-                if (array[i][j - 1] === 1) count++;
-                if (array[i][j + 1] === 1) count++;
-                if (array[i - 1][j - 1] === 1) count++;
-                if (array[i + 1][j - 1] === 1) count++;
-                if (array[i - 1][j + 1] === 1) count++;
-                if (array[i + 1][j + 1] === 1) count++;
+                if (array[i - 1][j] === '💥') count++;
+                if (array[i + 1][j] === '💥') count++;
+                if (array[i][j - 1] === '💥') count++;
+                if (array[i][j + 1] === '💥') count++;
+                if (array[i - 1][j - 1] === '💥') count++;
+                if (array[i + 1][j - 1] === '💥') count++;
+                if (array[i - 1][j + 1] === '💥') count++;
+                if (array[i + 1][j + 1] === '💥') count++;
                 dataBaseAliveCells.push(new Cell(i, j, count));
             }
         }
@@ -29,16 +29,16 @@ export const deathDataBaseNeighbours = (array) => {
     let dataBaseDeathCells = [];
     for (let i = 1; i < array.length - 1; i++) {
         for (let j = 1; j < array.length + 1; j++) {
-            if (array[i][j] === 0) {
+            if (array[i][j] === '🌕') {
                 let count = 0;
-                if (array[i - 1][j] === 1) count++;
-                if (array[i + 1][j] === 1) count++;
-                if (array[i][j - 1] === 1) count++;
-                if (array[i][j + 1] === 1) count++;
-                if (array[i - 1][j - 1] === 1) count++;
-                if (array[i + 1][j - 1] === 1) count++;
-                if (array[i - 1][j + 1] === 1) count++;
-                if (array[i + 1][j + 1] === 1) count++;
+                if (array[i - 1][j] === '💥') count++;
+                if (array[i + 1][j] === '💥') count++;
+                if (array[i][j - 1] === '💥') count++;
+                if (array[i][j + 1] === '💥') count++;
+                if (array[i - 1][j - 1] === '💥') count++;
+                if (array[i + 1][j - 1] === '💥') count++;
+                if (array[i - 1][j + 1] === '💥') count++;
+                if (array[i + 1][j + 1] === '💥') count++;
                 dataBaseDeathCells.push(new Cell(i, j, count));
             }
         }
